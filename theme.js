76f1e99f7e5c11eb36950f6af5ff93d58d7bbc68
Buba2704 <75638015+Buba2704@users.noreply.github.com>
2022-07-14 18:30:55 +0200

    css izmene boja

---
+++ theme.js
@@ -0,0 +1,30 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+
+const theme = createMuiTheme({
+    typography: {
+      useNextVariants: true,
+    },
+    palette: {
+      primary: {
+      light: '#4f83cc',
+      main: '#FFB6C1',
+      dark: '#002f6c',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#9fffe0',
+      main: '#ff8c00',
+      dark: '#2bbd7e',
+      contrastText: '#000',
+    },
+      openTitle: '#002f6c',
+      protectedTitle: '#2bbd7e',
+      type: 'light'
+    }
+
+    
+
+    
+  })
+
+  export default theme
